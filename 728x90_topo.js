162d7bb08c38c8ce7407293164063581e8cbaf42
mavelinodasilva <44980408+mavelinodasilva@users.noreply.github.com>
2019-06-12 12:08:04 -0300

    Update 728x90_topo.js

--- 728x90_topo.js
+++ 728x90_topo.js
@@ -1,42 +1,35 @@
-//
-var images_728x90 = [], index_728x90 = 0;
-//
-images_728x90[0] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/O8551550G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-IeXlpCu4yA4/XE8YoPfdOVI/AAAAAAAAAC8/HQI4zk5xvDEmvFxxjqPFuJTEO5dAJ6bLQCLcBGAs/s1600/O8551550G_images_728x90.gif' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
+var banner = [
+["https://go.hotmart.com/A5166248S?src=rotator_topo", "https://3.bp.blogspot.com/-tkkdrWqNUDc/XE8YmdVAZ8I/AAAAAAAAACs/yCNwBTFbQMQ6ZLqekYOzg12BhapDdfBBQCLcBGAs/s1600/A5166248S_images_728x90.jpg"],
+["https://go.hotmart.com/B7368341I?src=rotator_topo", "https://3.bp.blogspot.com/-c9X1_NChFpQ/XE8aOOQPYAI/AAAAAAAAAD0/2A_OZSMGtQYPcCYvcCHtCkWhD0RST8ZNgCLcBGAs/s1600/I2938479M_images_728x90.jpg"],
+["https://go.hotmart.com/B7460556X?src=rotator_topo", "https://2.bp.blogspot.com/-r-JMSiHEo_c/XE8YmQeMvbI/AAAAAAAAACk/kvfJB-acaAMw7RLEo2fu8Ch1nIT0A5REgCLcBGAs/s1600/B7460556X_images_728x90.jpg"],
+["https://go.hotmart.com/H3004461M?src=rotator_topo", "https://4.bp.blogspot.com/-uSfO6VjCUbI/XE8YmTsgeyI/AAAAAAAAACo/y70lMM7OdMUJC2sIDWMF4JaU-vhKGytfwCLcBGAs/s1600/H3004461M_images_728x90.jpg"],
+["https://go.hotmart.com/I2938479M?src=rotator_topo", "https://3.bp.blogspot.com/-c9X1_NChFpQ/XE8aOOQPYAI/AAAAAAAAAD0/2A_OZSMGtQYPcCYvcCHtCkWhD0RST8ZNgCLcBGAs/s1600/I2938479M_images_728x90.jpg"],
+["https://go.hotmart.com/I4928943O?src=rotator_topo", "https://1.bp.blogspot.com/-qnH2GnKGbrU/XE8YmzlU5vI/AAAAAAAAACw/-XufYIIrN8MgVvBlaaGla3KCB2bBmr8HwCLcBGAs/s1600/I4928943O_images_728x90.jpg"],
+["https://go.hotmart.com/I7349246G?src=rotator_topo", "https://3.bp.blogspot.com/-QcI-f0grml0/XE8YnSDBlBI/AAAAAAAAAC0/KZX5dMDoPgYs_aFE8-2NSjdMc8vNSs3vACLcBGAs/s1600/I7349246G_images_728x90.jpg"],
+["https://go.hotmart.com/M5085367G?src=rotator_topo", "https://3.bp.blogspot.com/-9m0UWMs5LL8/XE8Yn4BskyI/AAAAAAAAAC4/7H_IjuioPak23hFSGRsdrlkINsxGZ1FJQCLcBGAs/s1600/M5085367G_images_728x90.jpg"],
+["https://go.hotmart.com/O5209957G?src=rotator_topo", "https://1.bp.blogspot.com/-rMPEpZhEdsk/XE8aOIa1ERI/AAAAAAAAADs/XYP-8fSnTzAdJSsJxSGhKT-UQAI_WyBCgCLcBGAs/s1600/O5209957G_images_728x90.jpg"],
+["https://go.hotmart.com/O8551550G?src=rotator_topo", "https://4.bp.blogspot.com/-IeXlpCu4yA4/XE8YoPfdOVI/AAAAAAAAAC8/HQI4zk5xvDEmvFxxjqPFuJTEO5dAJ6bLQCLcBGAs/s1600/O8551550G_images_728x90.gif"],
+["https://go.hotmart.com/P4619028G?src=rotator_topo", "https://1.bp.blogspot.com/-Pe7lIPLqnx8/XE8aOXUbHoI/AAAAAAAAAD4/Q3RiXtPwrq0aeJYNeBYdG_TUJsBqEVYkgCLcBGAs/s1600/P4619028G_images_728x90.jpg"],
+["https://go.hotmart.com/P5127414M?src=rotator_topo", "https://3.bp.blogspot.com/-AI6MdoeBHnI/XE8YoUuUTNI/AAAAAAAAADA/Xd98Nd72EhQT8Z3VtP24SUKZn5H5xfgwACLcBGAs/s1600/P5127414M_images_728x90.jpg"],
+["https://go.hotmart.com/P5306343E?src=rotator_topo", "https://4.bp.blogspot.com/-9FSskvioWF4/XE8aOoOzUfI/AAAAAAAAAD8/wg1krpWyoJsYfyhW1FK_6xs2tWPlGPJpgCLcBGAs/s1600/P5306343E_images_728x90.jpg"],
+["https://go.hotmart.com/S5255545V?src=rotator_topo", "https://2.bp.blogspot.com/-NkOr22ybeDc/XE8Yo1C55AI/AAAAAAAAADE/lDNrb63SSg0YCW2L-MFpi0vujBb6yS9yACLcBGAs/s1600/S5255545V_images_728x90.jpg"],
+["https://go.hotmart.com/U3615855M?src=rotator_topo", "https://1.bp.blogspot.com/-5FLpH4g3x2c/XE8Yo2w1c1I/AAAAAAAAADI/QylVWwb0DTovMvOJzIpUcovGiOuGYpxXgCLcBGAs/s1600/U3615855M_images_728x90.jpg"],
+["https://go.hotmart.com/U4805487K?src=rotator_topo", "https://3.bp.blogspot.com/-arfmL2qL3yo/XE8YpJaEytI/AAAAAAAAADM/1guJa5b8S5w2vktx4FPmP1omF8eMO09EQCLcBGAs/s1600/U4805487K_images_728x90.jpg"],
+["https://go.hotmart.com/W5065217K?src=rotator_topo", "https://3.bp.blogspot.com/-zhE_C2MVaso/XE8YpMCus7I/AAAAAAAAADQ/puevLRWt1V4nrPEBs7UdrExavf3AgwP3gCLcBGAs/s1600/W5065217K_images_728x90.jpg"],
+["https://go.hotmart.com/X7092466A?src=rotator_topo", "https://3.bp.blogspot.com/-edpWsLM2aeE/XE8aPEF8GlI/AAAAAAAAAEA/d_sPRKlyckgdqf1HFYlmDqH68OVGIf1HwCLcBGAs/s1600/X7092466A_images_728x90.jpg"]
+];
 
-images_728x90[1] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P4619028G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-Pe7lIPLqnx8/XE8aOXUbHoI/AAAAAAAAAD4/Q3RiXtPwrq0aeJYNeBYdG_TUJsBqEVYkgCLcBGAs/s1600/P4619028G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
+function shuffle(a) {
+	var j, x, i;
+	for (i = a.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+	return a;
+}
 
-images_728x90[2] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/B7368341I?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-8w3_q0CxD-s/XE8aOIcf82I/AAAAAAAAADw/e_dWl2q9KI0irhWxeWTjOFdw6gYgtKoHgCLcBGAs/s1600/B7368341I_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
+shuffle(banner);
 
-images_728x90[3] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I2938479M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-c9X1_NChFpQ/XE8aOOQPYAI/AAAAAAAAAD0/2A_OZSMGtQYPcCYvcCHtCkWhD0RST8ZNgCLcBGAs/s1600/I2938479M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[4] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P5306343E?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-9FSskvioWF4/XE8aOoOzUfI/AAAAAAAAAD8/wg1krpWyoJsYfyhW1FK_6xs2tWPlGPJpgCLcBGAs/s1600/P5306343E_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[5] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/O5209957G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-rMPEpZhEdsk/XE8aOIa1ERI/AAAAAAAAADs/XYP-8fSnTzAdJSsJxSGhKT-UQAI_WyBCgCLcBGAs/s1600/O5209957G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[6] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/X7092466A?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-edpWsLM2aeE/XE8aPEF8GlI/AAAAAAAAAEA/d_sPRKlyckgdqf1HFYlmDqH68OVGIf1HwCLcBGAs/s1600/X7092466A_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[7] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/S5255545V?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://2.bp.blogspot.com/-NkOr22ybeDc/XE8Yo1C55AI/AAAAAAAAADE/lDNrb63SSg0YCW2L-MFpi0vujBb6yS9yACLcBGAs/s1600/S5255545V_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[8] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/B7460556X?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://2.bp.blogspot.com/-r-JMSiHEo_c/XE8YmQeMvbI/AAAAAAAAACk/kvfJB-acaAMw7RLEo2fu8Ch1nIT0A5REgCLcBGAs/s1600/B7460556X_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[9] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/M5085367G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-9m0UWMs5LL8/XE8Yn4BskyI/AAAAAAAAAC4/7H_IjuioPak23hFSGRsdrlkINsxGZ1FJQCLcBGAs/s1600/M5085367G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[10] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I4928943O?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-qnH2GnKGbrU/XE8YmzlU5vI/AAAAAAAAACw/-XufYIIrN8MgVvBlaaGla3KCB2bBmr8HwCLcBGAs/s1600/I4928943O_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[11] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/H3004461M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-uSfO6VjCUbI/XE8YmTsgeyI/AAAAAAAAACo/y70lMM7OdMUJC2sIDWMF4JaU-vhKGytfwCLcBGAs/s1600/H3004461M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[12] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P5127414M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-AI6MdoeBHnI/XE8YoUuUTNI/AAAAAAAAADA/Xd98Nd72EhQT8Z3VtP24SUKZn5H5xfgwACLcBGAs/s1600/P5127414M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[13] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/A5166248S?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-tkkdrWqNUDc/XE8YmdVAZ8I/AAAAAAAAACs/yCNwBTFbQMQ6ZLqekYOzg12BhapDdfBBQCLcBGAs/s1600/A5166248S_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[14] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/W5065217K?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-zhE_C2MVaso/XE8YpMCus7I/AAAAAAAAADQ/puevLRWt1V4nrPEBs7UdrExavf3AgwP3gCLcBGAs/s1600/W5065217K_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[15] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/U3615855M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-5FLpH4g3x2c/XE8Yo2w1c1I/AAAAAAAAADI/QylVWwb0DTovMvOJzIpUcovGiOuGYpxXgCLcBGAs/s1600/U3615855M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[16] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I7349246G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-QcI-f0grml0/XE8YnSDBlBI/AAAAAAAAAC0/KZX5dMDoPgYs_aFE8-2NSjdMc8vNSs3vACLcBGAs/s1600/I7349246G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-
-images_728x90[17] = "<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/U4805487K?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-arfmL2qL3yo/XE8YpJaEytI/AAAAAAAAADM/1guJa5b8S5w2vktx4FPmP1omF8eMO09EQCLcBGAs/s1600/U4805487K_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>";
-//
-index_728x90 = Math.floor ( Math.random() * images_728x90.length );
-document.write ( images_728x90[index_728x90] );
-//
+document.getElementById('ad-container').innerHTML = '<a href="'+banner[0][0]+'" target="_blank" rel="nofollow" title="CLIQUE AQUI PARA SABER MAIS..."><img src="'+banner[0][1]+'" style="width:100%;" alt="CLIQUE AQUI PARA SABER MAIS..." /></a>';
