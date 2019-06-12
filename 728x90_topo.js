04114732c033ab7df7a73c18e1203688faf0eadd
mavelinodasilva <44980408+mavelinodasilva@users.noreply.github.com>
2019-06-12 11:56:52 -0300

    Add files via upload

---
+++ 728x90_topo.js
@@ -0,0 +1,42 @@
+//
+var images_728x90 = [], index_728x90 = 0;
+//
+images_728x90[0] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/O8551550G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-IeXlpCu4yA4/XE8YoPfdOVI/AAAAAAAAAC8/HQI4zk5xvDEmvFxxjqPFuJTEO5dAJ6bLQCLcBGAs/s1600/O8551550G_images_728x90.gif' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[1] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P4619028G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-Pe7lIPLqnx8/XE8aOXUbHoI/AAAAAAAAAD4/Q3RiXtPwrq0aeJYNeBYdG_TUJsBqEVYkgCLcBGAs/s1600/P4619028G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[2] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/B7368341I?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-8w3_q0CxD-s/XE8aOIcf82I/AAAAAAAAADw/e_dWl2q9KI0irhWxeWTjOFdw6gYgtKoHgCLcBGAs/s1600/B7368341I_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[3] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I2938479M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-c9X1_NChFpQ/XE8aOOQPYAI/AAAAAAAAAD0/2A_OZSMGtQYPcCYvcCHtCkWhD0RST8ZNgCLcBGAs/s1600/I2938479M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[4] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P5306343E?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-9FSskvioWF4/XE8aOoOzUfI/AAAAAAAAAD8/wg1krpWyoJsYfyhW1FK_6xs2tWPlGPJpgCLcBGAs/s1600/P5306343E_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[5] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/O5209957G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-rMPEpZhEdsk/XE8aOIa1ERI/AAAAAAAAADs/XYP-8fSnTzAdJSsJxSGhKT-UQAI_WyBCgCLcBGAs/s1600/O5209957G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[6] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/X7092466A?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-edpWsLM2aeE/XE8aPEF8GlI/AAAAAAAAAEA/d_sPRKlyckgdqf1HFYlmDqH68OVGIf1HwCLcBGAs/s1600/X7092466A_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[7] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/S5255545V?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://2.bp.blogspot.com/-NkOr22ybeDc/XE8Yo1C55AI/AAAAAAAAADE/lDNrb63SSg0YCW2L-MFpi0vujBb6yS9yACLcBGAs/s1600/S5255545V_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[8] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/B7460556X?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://2.bp.blogspot.com/-r-JMSiHEo_c/XE8YmQeMvbI/AAAAAAAAACk/kvfJB-acaAMw7RLEo2fu8Ch1nIT0A5REgCLcBGAs/s1600/B7460556X_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[9] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/M5085367G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-9m0UWMs5LL8/XE8Yn4BskyI/AAAAAAAAAC4/7H_IjuioPak23hFSGRsdrlkINsxGZ1FJQCLcBGAs/s1600/M5085367G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[10] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I4928943O?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-qnH2GnKGbrU/XE8YmzlU5vI/AAAAAAAAACw/-XufYIIrN8MgVvBlaaGla3KCB2bBmr8HwCLcBGAs/s1600/I4928943O_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[11] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/H3004461M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://4.bp.blogspot.com/-uSfO6VjCUbI/XE8YmTsgeyI/AAAAAAAAACo/y70lMM7OdMUJC2sIDWMF4JaU-vhKGytfwCLcBGAs/s1600/H3004461M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[12] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/P5127414M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-AI6MdoeBHnI/XE8YoUuUTNI/AAAAAAAAADA/Xd98Nd72EhQT8Z3VtP24SUKZn5H5xfgwACLcBGAs/s1600/P5127414M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[13] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/A5166248S?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-tkkdrWqNUDc/XE8YmdVAZ8I/AAAAAAAAACs/yCNwBTFbQMQ6ZLqekYOzg12BhapDdfBBQCLcBGAs/s1600/A5166248S_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[14] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/W5065217K?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-zhE_C2MVaso/XE8YpMCus7I/AAAAAAAAADQ/puevLRWt1V4nrPEBs7UdrExavf3AgwP3gCLcBGAs/s1600/W5065217K_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[15] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/U3615855M?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://1.bp.blogspot.com/-5FLpH4g3x2c/XE8Yo2w1c1I/AAAAAAAAADI/QylVWwb0DTovMvOJzIpUcovGiOuGYpxXgCLcBGAs/s1600/U3615855M_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[16] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/I7349246G?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-QcI-f0grml0/XE8YnSDBlBI/AAAAAAAAAC0/KZX5dMDoPgYs_aFE8-2NSjdMc8vNSs3vACLcBGAs/s1600/I7349246G_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+
+images_728x90[17] = &quot;<a alt='CLIQUE AQUI PARA SABER MAIS...' href='https://go.hotmart.com/U4805487K?src=rotator_topo' target='_blank' title='CLIQUE AQUI PARA SABER MAIS...'><img alt='CLIQUE AQUI PARA SABER MAIS...' src='https://3.bp.blogspot.com/-arfmL2qL3yo/XE8YpJaEytI/AAAAAAAAADM/1guJa5b8S5w2vktx4FPmP1omF8eMO09EQCLcBGAs/s1600/U4805487K_images_728x90.jpg' style='width:100%;' title='CLIQUE AQUI PARA SABER MAIS...'/></a>&quot;;
+//
+index_728x90 = Math.floor ( Math.random() * images_728x90.length );
+document.write ( images_728x90[index_728x90] );
+//
